4ddbf6f87f8abd80262f080a0ccc188acceec915
mattmartinez <mattmartinez816@gmail.com>
2017-01-25 11:48:09 -0800

    built signup get and post routes

--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,30 @@ var router = express.Router();
 var User = require('../models/user.js');
 var authHelpers = require('../helpers/auth.js')
 
+// User Index
+
+// User Show
+
+// Signup
 router.get('/signup', function(req, res){
+  res.render('users/signup.hbs');
 });
 
 router.post('/', authHelpers.createSecure, function(req, res){
+  var user = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password_digest: res.hashedPassword
+  });
+
+  user.save(function(err, user) {
+    if (err) { console.log(err); }
+
+    console.log(user);
+    res.redirect('/users');
+  })
 });
 
 module.exports = router;
