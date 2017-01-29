c443843e3db449cd60b01ec7b3d90e0f0e49fe60
mattmartinez <mattmartinez816@gmail.com>
2017-01-29 00:58:45 -0800

    created slideshow on index page

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
 console.log('Script File Connected');
+
+// Citation: https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/
+// Learned from code in link above to create slideshow
+$("#tripPics > div:git(0)").hide();
+
+setInterval(function() {
+  $('#tripPics > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#tripPics');
+}, 3000);
