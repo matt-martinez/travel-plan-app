705301a726e041452c90e095ea50966715c3359e
mattmartinez <mattmartinez816@gmail.com>
2017-01-29 19:35:26 -0800

    added user image slideshow to user page

--- public/script.js
+++ public/script.js
@@ -2,13 +2,13 @@ console.log('Script File Connected');
 
 // Citation: https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/
 // Learned from code in link above to create slideshow
-$("#tripPics > div:git(0)").hide();
+$("#userImages > div:git(0)").hide();
 
 setInterval(function() {
-  $('#tripPics > div:first')
+  $('#userImages > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
-    .appendTo('#tripPics');
+    .appendTo('#userImages');
 }, 3000);
